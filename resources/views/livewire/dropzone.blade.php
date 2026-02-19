47c06796f2fdcf893d3000c06b9c8ae78c570ccc
chris_redaelli <chris@belead.com>
2026-02-19 16:24:24 +0100

    progress redo

--- resources/views/livewire/dropzone.blade.php
+++ resources/views/livewire/dropzone.blade.php
@@ -1,13 +1,13 @@
 <div
-    x-cloak
-    x-data="dropzone({
+        x-cloak
+        x-data="dropzone({
         _this: @this,
         uuid: @js($uuid),
     })"
-    @dragleave.prevent="isDragging = false"
-    @dragover.prevent="isDragging = true"
-    @drop.prevent="onDrop"
-    class="dz-block dz-antialiased"
+        @dragleave.prevent="isDragging = false"
+        @dragover.prevent="isDragging = true"
+        @drop.prevent="onDrop"
+        class="dz-block dz-antialiased"
 >
     <div class="dz-flex dz-flex-col dz-items-start dz-h-full dz-w-full dz-justify-center dz-bg-transparent dz-dark:border-gray-600 dz-dark:hover:border-gray-500">
         @if(! is_null($error))
@@ -41,16 +41,16 @@
                 </div>
             </div>
             <input
-                x-ref="input"
-                type="file"
-                class="dz-hidden"
-                x-on:livewire-upload-start="isLoading = true"
-                x-on:livewire-upload-cancel="isLoading = false"
-                x-on:livewire-upload-finish="isLoading = false"
-                x-on:livewire-upload-error="console.log('livewire-dropzone upload error')"
-                x-on:change.prevent="onChange"
-                @if(! is_null($this->accept)) accept="{{ $this->accept }}" @endif
-                @if($multiple === true) multiple @endif
+                    x-ref="input"
+                    type="file"
+                    class="dz-hidden"
+                    x-on:livewire-upload-start="isLoading = true"
+                    x-on:livewire-upload-cancel="isLoading = false"
+                    x-on:livewire-upload-finish="isLoading = false"
+                    x-on:livewire-upload-error="console.log('livewire-dropzone upload error')"
+                    x-on:change.prevent="onChange"
+                    @if(! is_null($this->accept)) accept="{{ $this->accept }}" @endif
+                    @if($multiple === true) multiple @endif
             >
         </div>
 
@@ -78,7 +78,7 @@
                     <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                 </svg>
-                <span class="dz-sr-only">Chargement...</span>
+                <span class="dz-sr-only">Chargement...</span> <span x-text="progress"></span> %
                 <div @click="cancelUpload" class="dz-text-xs dz-md:text-sm dz-text-gray-800 dz-dark:text-gray-200 dz-hover:cursor-pointer dz-underline">Annuler l'envoi</div>
             </div>
         </div>
@@ -127,6 +127,7 @@
                 isDragging: false,
                 isLoading: false,
                 isCancelled: false,
+                progress: 0,
 
                 onChange(e) {
                     this.isCancelled = false
@@ -164,6 +165,7 @@
                     this.chunks = {};
                     this.uploadedChunks = {};
                     this.isLoading = false
+                    this.progress = 0
 
                     if (this.$refs.input) {
                         this.$refs.input.value = ''
@@ -213,7 +215,15 @@
                             if (this.isCancelled) {
                                 return resolve('cancelled')
                             }
+
                             this.uploadedChunks[fileId]++
+
+                            const totalChunks = Object.values(this.chunks).reduce((acc, chunks) => acc + chunks.length, 0);
+                            const uploadedChunks = Object.values(this.uploadedChunks).reduce((acc, count) => acc + count, 0);
+
+                            if (totalChunks > 0) {
+                                this.progress = Math.round((uploadedChunks / totalChunks) * 100);
+                            }
                             resolve()
                         }
 
